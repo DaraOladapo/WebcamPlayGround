518d77a784f97c385328d38ef89dc73147b3d6cd
Dara Oladapo <daraoladapo@outlook.com>
2019-01-06 01:40:05 +0100

    Capture Picture Success

--- WebcamPlayGround/MainWindow.xaml.cs
+++ WebcamPlayGround/MainWindow.xaml.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WebcamPlayGround
 {
@@ -23,6 +18,48 @@ namespace WebcamPlayGround
         public MainWindow()
         {
             InitializeComponent();
+            var videoDevicesList = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            //foreach (FilterInfo videoDevice in videoDevicesList)
+            //{
+            //    cmbVideoSource.Items.Add(videoDevice.Name);
+            //}
+            var videoSource = videoDevicesList.Count > 1 ? new VideoCaptureDevice(videoDevicesList[1].MonikerString) : new VideoCaptureDevice(videoDevicesList[0].MonikerString);
+            videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+            videoSource.Start();
+        }
+        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            try
+            {
+                BitmapImage bitmapImage;
+                using (var bitmap = (Bitmap)eventArgs.Frame.Clone())
+                {
+                    using (MemoryStream memory = new MemoryStream())
+                    {
+                        bitmap.Save(memory, ImageFormat.Png);
+                        memory.Position = 0;
+                        bitmapImage = new BitmapImage();
+                        bitmapImage.BeginInit();
+                        bitmapImage.StreamSource = memory;
+                        bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                        bitmapImage.EndInit();
+                    }
+                    //bi = bitmap;//.ToBitmapImage();
+                }
+                bitmapImage.Freeze(); // avoid cross thread operations and prevents leaks
+                Dispatcher.BeginInvoke(new ThreadStart(delegate { CamImage.Source = bitmapImage; }));
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Error on _videoSource_NewFrame:\n" + exc.Message, "Error");
+
+                //StopCamera();
+            }
+        }
+
+        private void OnTakePicture(object sender, RoutedEventArgs e)
+        {
+            SnappedImage.Source = CamImage.Source;
         }
     }
 }
